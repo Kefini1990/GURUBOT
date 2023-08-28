b7a30c450139752eafee6d0c4f17a1f66a24fc49
PHYLO1234 <143414416+PHYLO1234@users.noreply.github.com>
2023-08-28 19:11:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254736958034', 'KHALID💚', true],
-  ['923470027813', 'AHMAD ALI❤', true], 
+  ['254762925335', 'PHYLO😁', true],
+  ['254762925335', 'JOVIAL', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['254736958034','923470027813'] 
-global.prems = ['254736958034', '923470027813', '254736958034']
+global.mods = ['254762925335','923470027813'] 
+global.prems = ['254762925335', '923470027813', '254762925335']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
