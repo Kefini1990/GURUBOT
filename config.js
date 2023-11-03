6106a8fc8b9a8407bb5a9348d38c4ad7b0685378
Kefini1990 <139688399+Kefini1990@users.noreply.github.com>
2023-11-03 14:20:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254762925335', 'PHYLO😁', true],
-  ['254762925335', 'JOVIAL', true], 
+  ['254772942486', 'kevin', true],
+  ['254772942486', 'winnie', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['254762925335','923470027813'] 
-global.prems = ['254762925335', '923470027813', '254762925335']
+global.mods = ['254772942486','923470027813'] 
+global.prems = ['254772942486', '923470027813', '254772942486']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
